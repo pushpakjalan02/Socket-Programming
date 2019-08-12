3200394cbd0667f5007dc2837d1e8f7f0caeeef4
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-08-12 22:19:56 +0530

    Added Sending File from Client to Server

---
+++ Assignment 1/Server/Server.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+#include <arpa/inet.h>
+
+#define MAX_CLIENT 1
+#define Server_Port 17002
+#define MAX_MSG_LEN 1000
+
+int main(int argc, char *argv[]){
+	int serv_sock, new_serv_sock;
+	
+	int client_addr_len, recv_data_len;
+
+	struct sockaddr_in client_addr, server_addr;
+	char buffer[MAX_MSG_LEN];
+	
+	bzero((char*)&server_addr, sizeof(server_addr));
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr("192.168.125.5");
+	server_addr.sin_port = htons(Server_Port);
+
+	serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if(serv_sock < 0){
+		perror("Cannot open socket\n");
+		exit(1);
+	}
+
+	if(bind(serv_sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
+		perror("Cannot Bind Port");
+		exit(1);
+	}
+
+	listen(serv_sock, MAX_CLIENT);
+
+	while(1){
+		client_addr_len = sizeof(client_addr);
+		new_serv_sock = accept(serv_sock, (struct sockaddr *) &client_addr, &client_addr_len);
+		if(new_serv_sock < 0){
+			perror("Cannot accept connection");
+			exit(1);
+		}
+
+		memset(buffer, 0x0, MAX_MSG_LEN);
+
+		recv(new_serv_sock, buffer, sizeof(buffer), 0);
+
+		FILE *fp = fopen(buffer, "w");
+
+		memset(buffer, 0x0, MAX_MSG_LEN);
+
+		while(recv_data_len = recv(new_serv_sock, buffer, sizeof(buffer), 0)){
+			fputs(buffer, fp);
+			memset(buffer, 0x0, MAX_MSG_LEN);
+		}
+
+		fclose(fp);
+		close(new_serv_sock);
+	}
+}
