60cfde9074035ec5f465cbc0781efbdee4bbed1e
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-10-15 21:29:06 +0530

    Modified

---
+++ Assignment 5/Hamming.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc, char *argv){
+	struct message_dt message;
+	struct  checksum[12];
+	int i = 0;
+	char s[9];
+	printf("Enter Message%lu\n", sizeof(message));
+	scanf("%d", message);
+	for(i = 0; i < 8; i++){
+		message[i].digit = s[i]-'0';
+	}
+	printf("Enter Probability");
+}
