8173546ce430b453875bdfc88b0d0611f058f201
Pushpak Jalan <pushpakjalan02@gmail.com>
2019-10-19 12:10:36 +0530

    Completed Client

--- Assignment 5/Host 1/Host1_Program.c
+++ Assignment 5/Host 1/Host1_Program.c
@@ -23,8 +23,8 @@ int main(int argc, char *argv[]){
 
 	char buffer[MAX_MSG_LEN];
 
-	if(argc < 5){
-		printf("usage: %s <Server IP> <Server Port> <Filename_Client> <Filename_Server>", argv[0]);
+	if(argc < 3){
+		printf("usage: %s <Server IP> <Server Port>\n", argv[0]);
 		exit(1);
 	}
 
@@ -35,13 +35,13 @@ int main(int argc, char *argv[]){
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if(sock < 0){
-		perror("Cannot Open Socket");
+		perror("Cannot Open Socket\n");
 		exit(1);
 	}
 
 	connect_return_val = connect(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
 	if(connect_return_val < 0){
-		perror("Cannot Connect");
+		perror("Cannot Connect\n");
 		exit(1);
 	}
 
@@ -63,13 +63,15 @@ int main(int argc, char *argv[]){
 	// checksum length
 	int cs_len = i;
 
+	// Structure for placing bits created
 	struct msg_dt *cs_frame = (struct msg_dt*)calloc((msg_len + cs_len) * sizeof(struct msg_dt), 0);
 
-	//checksum_position_mark
+	// parity_bits_position_mark
 	for(j = cs_len; j >= 0 ; j--){
 		cs_frame[(int)pow(2,j) - 1].digit = 3;		
 	}
 
+	// message bits placed in cs_frame array
 	for(i = 0,j = 0; j < msg_len; i++, j++){
 		if(cs_frame[i].digit == 3){
 			j--;
@@ -88,6 +90,7 @@ int main(int argc, char *argv[]){
 	printf("Enter Probability");
 */
 
+	// Parity bit values created
 	i = 0;
 	while(i<=cs_len){
 		int inc = pow(2,i);	
@@ -110,31 +113,45 @@ int main(int argc, char *argv[]){
 		i++;
 	}
 
+
+	// Checksummed frame is displayed
 	printf("Checksummed Frame: ");
 	for(i = 0; i < cs_len + msg_len ; i++)
 		printf("%d", cs_frame[i].digit);
 
+	// Probability of error accepted and rounded to 2 decimal places
 	float probability;
-	printf("\n\nEnter Probability of error\n");
+	printf("\n\nEnter Probability of error: ");
 	scanf("%f", &probability);
 	probability = (int)(probability*100+0.5)/100.0;
 
+	// Random No.: 1 Generated to randomly select between error and no error
 	srand(time(0));
 
 	int rand_no_1 = (rand() % (100-1+1)) + 1;
-	printf("%d\n", rand_no_1);
-	if(rand_no_1 <= probability*100){
-		// Error
-		int rand_no_2 = (rand() % ((cs_len+msg_len-1) - 0 + 1)) + 0;
-		// Flip
-		cs_frame[rand_no_2].digit = cs_frame[rand_no_2].digit == 0 ? 1 : 0;
-		printf("%d\n", rand_no_2);
+	printf("Random No. 1: %d\n", rand_no_1);
 
+	// If Error required to be simulate, randomly select bit position.
+	if(rand_no_1 <= probability*100){
+
+		int rand_no_2 = (rand() % ((cs_len+msg_len-1) - 0 + 1)) + 0;
+		// Flip the bit position
+		cs_frame[rand_no_2].digit = cs_frame[rand_no_2].digit == 0 ? 1 : 0;
+		printf("Random No. 2:%d\n", rand_no_2);
 	}
 
+	memset(buffer, 0x0, MAX_MSG_LEN);
 	for(i = 0; i < cs_len + msg_len ; i++)
-		printf("%d   %d\n", i, cs_frame[i].digit);
+		buffer[i] = cs_frame[i].digit + '0';
 
+	buffer[i] = '\0';
+
+	// Display Frame with Error
+	printf("Error Frame sent: ");
+	printf("%s\n", buffer);
+
+	// Send Error Frame to Server
+	send(sock, buffer, strlen(buffer) + 1, 0);
 
 	// Ends Here
 
